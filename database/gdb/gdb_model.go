65a6b4ca4441cda0cf6b5c98037d07116f5ece5d
CyJaySong <cyjaysong@gmail.com>
2024-10-17 11:22:41 +0800

    Model默认Safe为false

--- database/gdb/gdb_model.go
+++ database/gdb/gdb_model.go
@@ -140,9 +140,6 @@ func (c *Core) Model(tableNameQueryOrStruct ...interface{}) *Model {
 		tableAliasMap: make(map[string]string),
 	}
 	m.whereBuilder = m.Builder()
-	if defaultModelSafe {
-		m.safe = true
-	}
 	return m
 }
 
